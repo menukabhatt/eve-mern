a89df0db79f883d094a95f39b7bddb6258aaecaa
Rabyn-Collab <babynshrestha64@gmail.com>
2024-09-30 15:32:41 +0545

    ch

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis sed sit velit facilis ipsam iusto nobis provident mollitia nulla assumenda perspiciatis nesciunt aliquid, doloremque beatae ad eaque cupiditate corrupti perferendis.</p>
+    </div>
   )
 }
 export default HomePage
