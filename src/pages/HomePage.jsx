30407303e9d9ebef2bbfa649c1d44b593ae93129
Rabyn-Collab <babynshrestha64@gmail.com>
2024-10-22 15:41:44 +0545

    hooks

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,31 @@
 import axios from "axios"
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
+  const [data, setData] = useState([]);
+  const getData = async () => {
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setData(response.data);
+    } catch (err) {
+    }
+  }
 
-
+  useEffect(() => {
+    getData();
+  }, []);
 
 
   return (
     <div className="p-5">
 
+      {data.map((post, i) => {
+        return <div key={post.id}>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
 
+        </div>
+      })}
 
 
     </div>
