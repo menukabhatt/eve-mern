a8fc4cd18f0cf1617fb8e1eda8171f751272932a
Rabyn-Collab <babynshrestha64@gmail.com>
2024-10-23 16:13:24 +0545

    formik

--- src/App.js
+++ src/App.js
@@ -4,6 +4,20 @@ import HomePage from "./pages/HomePage";
 
 const App = () => {
 
+
+  const person = {
+    name: 'ram',
+    age: 90,
+    add: {
+      j: 9,
+      b: {
+        c: 90
+      }
+    }
+  };
+
+  const { name, age, add: { j, b: { c } } } = person;
+
   const router = createBrowserRouter([
     {
       path: '/',
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,60 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { ListCard } from "../components/ListCard";
+import { CardCompo } from "../components/CardCompo";
+
 const HomePage = () => {
+
+  const [load, setLoad] = useState(false);
+  const [err, setErr] = useState();
+  const [data, setData] = useState();
+
+  const getData = async () => {
+    setLoad(true);
+    try {
+      const response = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php');
+      setLoad(false);
+      setData(response.data);
+    } catch (err) {
+      setLoad(false);
+      console.log(err);
+      setErr(err.message);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+
+
+  if (load) {
+    return <h1>Loading......</h1>
+  }
+
+  if (err) {
+    return <h1>{err}</h1>
+  }
+
+  console.log(data);
+
   return (
     <div>
+
+
+
+
+
+      {data && data.categories.map(({ strCategory, idCategory }, i) => {
+
+        return <CardCompo key={idCategory} strCategory={strCategory} />
+
+      })}
+
+
+
+
+
 
 
 
