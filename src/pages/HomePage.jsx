49c12a2d297b9a808690e04845f119cbefa2996c
Rabyn-Collab <babynshrestha64@gmail.com>
2024-10-25 15:59:04 +0545

    formik

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,42 @@
-const HomePage = () => {
-  return (
-    <div>
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { CardCompo } from "../components/CardCompo";
 
-      <h1>hjfghf</h1>
-      <p>jhgjhghj</p>
+
+const HomePage = () => {
+
+  const [data, setData] = useState();
+
+  const getData = async () => {
+    try {
+
+      const response = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php');
+      setData(response.data);
+    } catch (err) {
+
+    }
+
+  }
+
+
+  useEffect(() => {
+    getData();
+
+  }, []);
+
+
+  console.log(data);
+
+  return (
+    <div className="p-4 grid grid-cols-3 gap-5">
+
+      {data && data.categories.map((cata) => {
+        return <CardCompo key={cata.idCategory} age={90} image={cata.strCategoryThumb} />;
+      })}
+
+
+
+
 
 
 
