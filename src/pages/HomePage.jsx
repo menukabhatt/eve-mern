f06d5bffd3de7158ef08da1a5c98ca166bedb89a
Rabyn-Collab <babynshrestha64@gmail.com>
2024-11-05 16:09:40 +0545

    ch

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,44 @@
+import { Button, Input } from "@material-tailwind/react"
+import { useFormik } from "formik"
+
 const HomePage = () => {
+  const { values, handleChange, handleSubmit, } = useFormik({
+    initialValues: {
+      username: '',
+      email: '',
+    },
+    onSubmit: (val) => {
+      console.log(val);
+
+    }
+
+  });
+
   return (
-    <div>
+    <div className="max-w-[250px] p-4">
+
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <Input
+          onChange={handleChange}
+          label="Username"
+          name="username"
+          value={values.username}
+        />
+        <Input
+          onChange={handleChange}
+          label="Email"
+          name="email"
+          value={values.email}
+        />
+        <Button size="sm" type="submit">Submit</Button>
+
+      </form>
+
+
+
+
+
+
 
 
 
