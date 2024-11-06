107ace9b68d77472a047144de3921380b677effa
Rabyn-Collab <babynshrestha64@gmail.com>
2024-11-06 16:18:42 +0545

    ch

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,38 @@
-import { Button, Input } from "@material-tailwind/react"
+import { Button, Checkbox, Input, Option, Radio, Select, Textarea, Typography } from "@material-tailwind/react"
 import { useFormik } from "formik"
+import * as Yup from 'yup';
+
+const formSchema = Yup.object({
+  username: Yup.string().min(5).max(16).required('Required'),
+  email: Yup.string().matches(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).required(),
+  // gender: '',
+  // habits: [],
+  // country: '',
+  // message: ''
+});
+
+
 
 const HomePage = () => {
-  const { values, handleChange, handleSubmit, } = useFormik({
-    initialValues: {
-      username: '',
-      email: '',
-    },
-    onSubmit: (val) => {
-      console.log(val);
+  const {
+    values,
+    errors,
+    handleChange, handleSubmit, setFieldValue } = useFormik({
+      initialValues: {
+        username: '',
+        email: '',
+        gender: '',
+        habits: [],
+        country: '',
+        message: ''
+      },
+      onSubmit: (val) => {
+        console.log(val);
 
-    }
+      },
+      validationSchema: formSchema
 
-  });
+    });
 
   return (
     <div className="max-w-[250px] p-4">
@@ -24,12 +44,64 @@ const HomePage = () => {
           name="username"
           value={values.username}
         />
+        <p className="text-red-600">{errors.username}</p>
         <Input
           onChange={handleChange}
           label="Email"
           name="email"
           value={values.email}
         />
+
+        <div>
+          <Typography variant="h6">Select Your Gender</Typography>
+          <Radio
+            label='Male'
+            onChange={handleChange}
+            value={'Male'}
+            name="gender"
+          />
+          <Radio
+            label='Female'
+            onChange={handleChange}
+            value={'Female'}
+            name="gender"
+          />
+        </div>
+        <div>
+          <Typography variant="h6">Select Your Habbits</Typography>
+          <Checkbox
+            label='Dance'
+            onChange={handleChange}
+            value={'Dance'}
+            name="habits"
+          />
+          <Checkbox
+            label='Sing'
+            onChange={handleChange}
+            value={'Sing'}
+            name="habits"
+          />
+        </div>
+        <div>
+
+
+          <Select
+            onChange={(e) => setFieldValue('country', e)}
+            name='country'
+            label="Select Your Country">
+            <Option value="Nepal">Nepal</Option>
+            <Option value="China">China</Option>
+            <Option value="India">India</Option>
+          </Select>
+        </div>
+        <div>
+          <Textarea
+            name="message"
+            onChange={handleChange}
+            value={values.message}
+            label="type your message" />
+        </div>
+
         <Button size="sm" type="submit">Submit</Button>
 
       </form>
