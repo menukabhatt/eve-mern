59905a6fc625e80ca030a0ff4f10ff37627bdd97
Rabyn-Collab <babynshrestha64@gmail.com>
2024-11-15 16:12:10 +0545

    create css

---
+++ src/features/cocktail/cocktailApi.js
@@ -0,0 +1,42 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+
+// const m = () => {
+//   return {
+
+//   }
+// }
+
+export const cocktailApi = createApi({
+
+  reducerPath: 'cocktailApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://www.thecocktaildb.com/api/json/v1/1' }),
+
+  endpoints: (builder) => ({
+
+
+    cocktailCategory: builder.query({
+
+      query: () => ({
+        url: '/filter.php',
+        params: {
+          c: 'Cocktail'
+        },
+        method: 'GET'
+
+      })
+
+    })
+
+
+
+
+  })
+
+
+
+
+
+
+})
