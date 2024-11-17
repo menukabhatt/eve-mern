5c11f637565699d0df837d09e1331f0a2094a8ca
Rabyn-Collab <babynshrestha64@gmail.com>
2024-11-17 16:06:54 +0545

    create css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { useCocktailCategoryQuery } from "./features/cocktail/cocktailApi"
+import { ListCompo } from "./ui/ListCompo";
 
 const App = () => {
 
-  const { isLoading, error, isError, data } = useCocktailCategoryQuery();
+  const { isLoading, error, isError, data, isFetching } = useCocktailCategoryQuery();
+
   console.log(data);
 
 
 
   return (
     <div>
+
+      {data && <ListCompo drinks={data.drinks} />}
 
 
 
