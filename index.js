4cc6f5e323c5791b76a941ec206779c6d033cffd
Rabyn-Collab <babynshrestha64@gmail.com>
2024-11-24 15:49:03 +0545

    google

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-import express, { query } from 'express';
+import express from 'express';
 
 const app = express();
 const port = 5000;
 
 
 const products = [
-  { id: 1, title: 'product 1' },
-  { id: 2, title: 'product 2' },
-
-];
+  { id: 1, name: 'laptop', price: 50000 },
+  { id: 2, name: 'phone', price: 40000 },
+  { id: 3, name: 'tablet', price: 30000 }
+]
 app.get('/', (req, res) => {
 
-  const { id } = req.query;
-  const product = products.find((product) => product.id === Number(id));
-  return res.status(200).json(product);
+
+
 
 });
-
-app.get('/products', (req, res) => {
-  return res.status(200).json(products);
-})
 
 
 
