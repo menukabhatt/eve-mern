c19844ab89aa0baa75618763cac67772b1d6090a
Rabyn-Collab <babynshrestha64@gmail.com>
2024-12-02 15:56:00 +0545

    sdfkj

---
+++ src/app/apiUrl.js
@@ -0,0 +1,4 @@
+
+
+
+export const baseUrl = 'http://192.168.1.64:5000';
---
+++ src/features/auth/authApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { baseUrl } from "../../app/apiUrl";
+
+export const authApi = createApi({
+  reducerPath: 'authApi',
+  baseQuery: fetchBaseQuery({ baseUrl: baseUrl }),
+
+  endpoints: (builder) => ({
+
+
+    userLogin: builder.mutation({
+      query: (q) => ({
+        url: '/login',
+        body: q,
+        method: 'POST',
+
+      })
+    })
+
+
+
+  })
+
+});
